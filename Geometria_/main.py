7879d187207cc89c0019cbb270125d68f8139d4f
dauro95 <angel.sen.jimenez@gmail.com>
2022-03-18 22:22:40 +0100

    ejercicion_finalizado

--- Geometria_/main.py
+++ Geometria_/main.py
@@ -1,5 +1,5 @@
-from Geometria_.model.Geometria import Geometria
-from Geometria_.view.View import View
+from model.Geometria import Geometria
+from view.View import View
 
 if __name__ == '__main__':
     g = Geometria(2.00, 3.10, 4.00)
